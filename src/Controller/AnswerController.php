5d4235132732104844e9ccb79dcf1be6088591ac
StijnFreshheads <stijn.albers@freshheads.com>
2022-04-11 13:47:45 +0200

    first commit

---
+++ src/Controller/AnswerController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Answer;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnswerController extends AbstractController
+{
+    /**
+     * @Route("/answers/{id}/vote", methods="POST", name="answer_vote")
+     */
+    public function answerVote(Answer $answer, LoggerInterface $logger, Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+        $direction = $data['direction'] ?? 'up';
+        
+        // use real logic here to save this to the database
+        if ($direction === 'up') {
+            $logger->info('Voting up!');
+            $answer->setVotes($answer->getVotes() + 1);
+        } else {
+            $logger->info('Voting down!');
+            $answer->setVotes($answer->getVotes() - 1);
+        }
+
+        return $this->json(['votes' => $currentVoteCount]);
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Question;
+use App\Factory\AnswerFactory;
+use App\Factory\QuestionFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $questions = QuestionFactory::new()->createMany(20);
+
+        QuestionFactory::new()
+            ->unpublished()
+            ->many(5)
+            ->create()
+        ;
+
+        AnswerFactory::createMany(100, function() use ($questions) {
+            return [
+                'question' => $questions[array_rand($questions)]
+            ];
+        });
+
+        $manager->flush();
+    }
+}
